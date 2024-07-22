ac7a173de36857fb28d5f7e7fb9d003354800624
sayed younis <elsaydseif17@gmail.com>
2024-07-22 22:53:25 +0300

    weather condition image changes with conditon comes from api

--- lib/Views/home_page.dart
+++ lib/Views/home_page.dart
@@ -22,7 +22,6 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
     weatherServices = WeatherServices();
-    future = weatherServices.getWeather(city: city);
   }
 
   void _updateWeather() {
@@ -33,145 +32,157 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return BackdropFilter(
-      filter: ImageFilter.blur(),
-      child: Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(
-          elevation: 0.0,
-          backgroundColor: Colors.transparent,
-          actions: [
-            IconButton(
-              icon: const Icon(
-                Icons.search,
-                size: 30,
-              ),
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  barrierDismissible: false, // user must tap button!
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: const Text('Search For Your City'),
-                      content: SingleChildScrollView(
-                        child: TextField(
-                          onSubmitted: (value) {
-                            setState(() {
-                              city = value;
-                              _updateWeather();
-                              Navigator.of(context).pop();
-                            });
-                          },
-                          decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                      ),
-                      actions: <Widget>[
-                        TextButton(
-                          child: const Text('Search'),
-                          onPressed: () {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.transparent,
+        actions: [
+          IconButton(
+            icon: const Icon(
+              Icons.search,
+              size: 30,
+            ),
+            onPressed: () {
+              showDialog(
+                context: context,
+                barrierDismissible: false, // user must tap button!
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text('Search For Your City'),
+                    content: SingleChildScrollView(
+                      child: TextField(
+                        onSubmitted: (value) {
+                          setState(() {
+                            city = value;
                             _updateWeather();
                             Navigator.of(context).pop();
-                          },
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-            ),
-          ],
-        ),
-        body: SingleChildScrollView(
-          child: FutureBuilder<WeatherModel>(
-            future: future,
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
-                return const Center(
-                  child: Padding(
-                    padding: EdgeInsets.symmetric(vertical: 300.0),
-                    child: CircularProgressIndicator(),
-                  ),
-                );
-              } else if (snapshot.hasData) {
-                return Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          CustomText(
-                            text: snapshot.data!.country,
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ],
-                      ),
-                      Image.asset(
-                        'assets/cloudy.png',
-                        cacheHeight: 250,
-                      ),
-                      CustomText(
-                        text: snapshot.data!.condition,
-                        fontSize: 40,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          CustomData(
-                            value: ' ${snapshot.data!.temp.round()}',
-                            image: 'assets/wind.png',
-                            unit: 'mph',
-                            about: 'Wind',
-                          ),
-                          CustomData(
-                            value: ' ${snapshot.data!.temp.round()}°',
-                            image: 'assets/celsius.png',
-                            about: 'Temperature',
-                          ),
-                          CustomData(
-                            value: '${snapshot.data!.humidity}%',
-                            image: 'assets/water-drops.png',
-                            about: 'humidity',
-                          ),
-                        ],
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.symmetric(vertical: 20.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            CustomText(
-                              text: 'Today',
-                              fontSize: 25,
-                              fontWeight: FontWeight.w500,
-                            ),
-                          ],
+                          });
+                        },
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(),
                         ),
                       ),
-                      SizedBox(
-                        height: 200,
-                        child: ListView.builder(
-                          scrollDirection: Axis.horizontal,
-                          itemCount: snapshot.data!.timeList.length,
-                          itemBuilder: (context, index) {
-                            return CustomWeatherCard(
-                              time: snapshot.data!.timeList[index],
-                              temp: snapshot.data!.tempList[index],
-                            );
-                          },
-                        ),
+                    ),
+                    actions: <Widget>[
+                      TextButton(
+                        child: const Text('Search'),
+                        onPressed: () {
+                          _updateWeather();
+                          Navigator.of(context).pop();
+                        },
                       ),
                     ],
-                  ),
-                );
-              } else {
-                return Text('Has Error: ${snapshot.error}');
-              }
+                  );
+                },
+              );
             },
           ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: FutureBuilder<WeatherModel>(
+          future: future,
+          builder: (context, snapshot) {
+            String giveWeather() {
+              if (snapshot.data!.condition == 'Sunny') {
+                return 'assets/sun.png';
+              } else if (snapshot.data!.condition == 'Patchy rain nearby') {
+                return 'assets/heavy-rain.png';
+              } else if (snapshot.data!.condition == 'Clear') {
+                return 'assets/cloudy.png';
+              } else if (snapshot.data!.condition == 'Clear') {
+                return 'assets/cloudy.png';
+              } else if (snapshot.data!.condition == 'Light rain shower') {
+                return 'assets/light-rain.png';
+              } else {
+                return 'assets/cloudy.png';
+              }
+            }
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: Padding(
+                  padding: EdgeInsets.symmetric(vertical: 300.0),
+                  child: CircularProgressIndicator(),
+                ),
+              );
+            } else if (snapshot.hasData) {
+              return Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        CustomText(
+                          text: snapshot.data!.country,
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ],
+                    ),
+                    Image.asset(
+                      giveWeather(),
+                      cacheHeight: 250,
+                    ),
+                    CustomText(
+                      text: snapshot.data!.condition,
+                      fontSize: 40,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        CustomData(
+                          value: ' ${snapshot.data!.temp.round()}',
+                          image: 'assets/wind.png',
+                          unit: 'mph',
+                          about: 'Wind',
+                        ),
+                        CustomData(
+                          value: ' ${snapshot.data!.temp.round()}°',
+                          image: 'assets/celsius.png',
+                          about: 'Temperature',
+                        ),
+                        CustomData(
+                          value: '${snapshot.data!.humidity}%',
+                          image: 'assets/water-drops.png',
+                          about: 'humidity',
+                        ),
+                      ],
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.symmetric(vertical: 20.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          CustomText(
+                            text: 'Today',
+                            fontSize: 25,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 200,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: snapshot.data!.timeList.length,
+                        itemBuilder: (context, index) {
+                          return CustomWeatherCard(
+                            time: snapshot.data!.timeList[index],
+                            temp: snapshot.data!.tempList[index],
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              );
+            } else {
+              return Text('Has Error: ${snapshot.error}');
+            }
+          },
         ),
       ),
     );
